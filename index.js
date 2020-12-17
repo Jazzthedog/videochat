28b30471c322fa675daecedf494897809d7f5708
Jazzthedog <jazzthedog.bowwow@gmail.com>
2020-12-16 22:35:34 -0500

    1st signaling approach with socket.io and room

--- index.js
+++ index.js
@@ -1,12 +1,51 @@
 const express = require('express');
-const socket = require('socket.io');
-
-var app = express();
+const socket = require("socket.io");
+const app = express();
 
 // callback function
 var server = app.listen(4000, function() {
     console.log("Listening on port 4000");
 });
 
-// interesting use of this method to point to a folder?? this will render index.html automatically
+// interesting use of this method to point to a folder?? this will render index.html by default.
 app.use(express.static('public'));
+
+// wrap the server with socket.io , not 100% sure why we need this fully?
+var io = socket(server);
+
+io.on("connection", function(socket) {
+    console.log("User Connected :", socket.id);
+
+    socket.on("join", function(roomName) {
+        // each room can only have 2 particants
+        // each room name is unique, so if room doesn't exist, we create one. 
+        // If roomname already exists, let user join it.
+        // If the room already has 2 people in it, let user not join.
+
+        // get a map or list of existing websocket connections
+        var rooms = io.sockets.adapter.rooms;
+        console.log("rooms:", rooms);
+
+        // check to see if room already exists
+        //var room = io.sockets.adapter.rooms.get(roomName);
+        //console.log("room:", room);
+
+        var room = rooms.get(roomName);
+
+        // if room is undefined, we will dynamically create one.
+        if (room == undefined) {
+            // no room exists so create on
+            socket.join(roomName);
+            console.log("Room Created");
+            console.log("room:", io.sockets.adapter.rooms.get(roomName) );
+
+        }
+        else if (room.size == 1) {
+            socket.join(roomName);
+            console.log("Room Joined");
+        }
+        else {
+            console.log("Room full (2 people max) for now");
+        }
+    });
+});
--- public/chat.js
+++ public/chat.js
@@ -1,3 +1,5 @@
+var socket = io.connect("http://localhost:4000");
+
 var divVideoChatLobby = document.getElementById("video-chat-lobby");
 var divVideoChat = document.getElementById("video-chat-room");
 var joinButton = document.getElementById("join");
@@ -5,16 +7,22 @@ var userVideo = document.getElementById("user-video");
 var peerVideo = document.getElementById("peer-video");
 var roomInput = document.getElementById("roomName");
 
+console.log("socket:", socket);
+
 joinButton.addEventListener('click', function() {
     if (roomInput.value == "") {
         alert("Please enter a room name");
     }
     else {
+
+        // we need to let the server know that a user is trying to enter our room. Raise an 'event' and pass the roomname with it.
+        socket.emit("join", roomInput.value);
+
         // navigator.mediaDevices.getUserMedia does NOT work!!!
         // navigator.getUserMedia() is a legacy method.
         navigator.getUserMedia(
         {
-            audio: true,
+            audio: false,
             video: { width: 1280, height: 720 },
         },
         function(stream) {
